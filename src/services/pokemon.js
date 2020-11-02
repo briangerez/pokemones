69a80705f376a9d99cee0fd15bdd42f7ae377ca7
Brian Gerez <brian.gerez@wolox.com.ar>
2020-11-02 17:05:49 -0300

    some files removed

--- src/services/pokemon.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/extensions */
-import api from './api.js';
-
-// eslint-disable-next-line import/prefer-default-export
-export const getPokemon = (name) => api.get(`v2/pokemon/${name}`);
